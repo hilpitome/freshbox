07fbfc30901a755ee3c7de37a9e98cc88d9a80c8
Sylvia <sylviakieha@gmail.com>
2015-10-15 01:49:15 +0300

    edited image sizes on modals

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
     :small => '240x240>', # images on category view
-    :product => '320x320>', # full product image
+    :product => '230x230>', # full product image
     :large => '600x600>' # light box image
   }
 end
